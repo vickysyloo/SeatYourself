b2b15bf0d5442dd618beaa1a5a81393470fc7c63
Vicky L <vicky.sy.loo@gmail.com>
2016-04-14 14:39:54 -0400

    Initial commit, Users model generated

---
+++ SeatYourself/db/migrate/20160414183929_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.text :first_name
+      t.text :last_name
+      t.text :email
+      t.string :phone
+
+      t.timestamps null: false
+    end
+  end
+end
